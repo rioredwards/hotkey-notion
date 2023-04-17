34178446f3f98038229bf90c63d8ccc41e91fd02
Rio Edwards <rioredwards@gmail.com>
2023-04-17 11:27:42 -0700

    Successfully connect to db

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,14 @@ async function main() {
   const notion = new Client({
     auth: process.env.NOTION_TOKEN,
   });
+  const databaseId = process.env.NOTION_DATABASE_ID!;
+
+  // const database = await notion.databases.retrieve({
+  //   database_id: databaseId,
+  // });
 
   const response = await notion.databases.query({
-    database_id: "FIXME",
+    database_id: databaseId,
   });
 
   console.log("Got response:", response);
